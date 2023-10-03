f895c2048c4210813252dc2f27ce5e96ebf7a73b
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-03 11:31:13 +0530

    made auth username and email unique and migrated new user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.unregister(Group)
+admin.site.register(User)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
