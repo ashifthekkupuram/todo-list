db04ddff1fc30816be2c39b0b0bf86ad7feeed48
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-03 19:16:16 +0530

    created new task model and migrated

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    discription = models.TextField(null=True,blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} by {self.author.username}'
