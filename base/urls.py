34b3d9e8ee4020fa8d05e0e1690b0e5c4ffec06a
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-07 15:28:29 +0530

    made update task form and fixed home list listing order

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView,TaskView,TaskCreate
+from .views import HomeView,TaskView,TaskCreate,TaskUpdate
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
     path('task/<int:pk>/',TaskView.as_view(),name='task'),
     path('task-create/',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>',TaskUpdate.as_view(),name='task-update'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy,reverse
 from django.shortcuts import render
 
-from django.views.generic import ListView,DetailView, CreateView
+from django.views.generic import ListView,DetailView, CreateView,UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Task
 
@@ -13,7 +13,7 @@ class HomeView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(author=self.request.user).order_by('-created','completed')
+        context['tasks'] = context['tasks'].filter(author=self.request.user).order_by('completed','-created')
         
         search_input = self.request.GET.get('search_input') or ''
         
@@ -44,4 +44,19 @@ class TaskCreate(LoginRequiredMixin, CreateView):
         task = form.save(commit=False)
         task.author = self.request.user
         task.save()
-        return super(TaskCreate, self).form_valid(form)        
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Task
+    fields = ['title','discription', 'completed']
+    context_object_name = 'task'
+    
+    def test_func(self):
+        task = self.get_object()
+        if task.author == self.request.user:
+            return True
+        else:
+            return False
+        
+    def get_success_url(self):
+        return reverse('task', kwargs={'pk':self.get_object().id})
