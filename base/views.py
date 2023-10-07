723759722f4613a5323a4d0b745cbf63ef0057df
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-07 14:26:31 +0530

    sorted task home list to newest first

--- base/views.py
+++ base/views.py
@@ -12,7 +12,7 @@ class HomeView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(author=self.request.user).order_by('completed')
+        context['tasks'] = context['tasks'].filter(author=self.request.user).order_by('-created','completed')
         
         search_input = self.request.GET.get('search_input') or ''
         
